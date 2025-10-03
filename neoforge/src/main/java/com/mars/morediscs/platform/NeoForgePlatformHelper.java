d460a564bd452cde7335f7bb71fada24e787ed66
Mars <ruzicka.maty@gmail.com>
2025-10-04 00:51:55 +0200

    updated to 1.21.9

--- neoforge/src/main/java/com/mars/morediscs/platform/NeoForgePlatformHelper.java
+++ neoforge/src/main/java/com/mars/morediscs/platform/NeoForgePlatformHelper.java
@@ -2,7 +2,6 @@ package com.mars.morediscs.platform;
 
 import com.mars.morediscs.platform.services.IPlatformHelper;
 import net.neoforged.fml.ModList;
-import net.neoforged.fml.loading.FMLLoader;
 
 public class NeoForgePlatformHelper implements IPlatformHelper {
 
@@ -14,10 +13,5 @@ public class NeoForgePlatformHelper implements IPlatformHelper {
     @Override
     public boolean isModLoaded(String modId) {
         return ModList.get().isLoaded(modId);
-    }
-
-    @Override
-    public boolean isDevelopmentEnvironment() {
-        return !FMLLoader.isProduction();
     }
 }
